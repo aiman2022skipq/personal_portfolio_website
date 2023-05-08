c0d1a216158027359ceccf8b392f325b82c74bfc
aim-t <namiatariq@gmail.com>
2023-05-08 18:06:13 +0500

    Complete Technologies Section

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,66 @@
-import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import React from "react";
+import { DiFirebase, DiReact, DiZend } from "react-icons/di";
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from "./TechnologiesStyles";
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+const Technologies = () => (
+  <Section id="tech">
+
+    <SectionDivider />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      Lorem ipsum dolor spagesit amet consectetur adipisicing elit. Maxime
+      mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+      voluptatum lab
+    </SectionText>
+
+    <List>
+      <ListItem>
+        <DiReact size="3rem"/>
+        <ListContainer>
+          <ListTitle>Frontend</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            React.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+
+      <ListItem>
+        <DiFirebase size="3rem"/>
+        <ListContainer>
+          <ListTitle>Backend</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Node
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      
+      <ListItem>
+        <DiZend size="3rem"/>
+        <ListContainer>
+          <ListTitle>UI/UX</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Figma
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+
+  </Section>
 );
 
 export default Technologies;
